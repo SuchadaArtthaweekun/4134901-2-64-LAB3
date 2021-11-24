4f9a5b1d6fd3bf3259fc4aab35e60d4a7dde9e63
SuchadaArtthaweekun <59986479+SuchadaArtthaweekun@users.noreply.github.com>
2021-11-24 13:03:43 +0700

    LAB3

---
+++ LAB3/hello-react/src/components/name.js
@@ -0,0 +1,11 @@
+function MyName() {
+    return(
+        <div className="MyName">
+            <div>Suchada Artthaweekun</div>
+            
+            <div>610112418050</div>
+        </div>
+        );
+}
+
+export default MyName
